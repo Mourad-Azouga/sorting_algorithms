de7531b8601089c347ff7437757dc2d98b89052f
Mourad-Azouga <mourad-azouga@outlook.fr>
2023-12-17 08:07:31 -0800

    Fixed some issues

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,24 +1,29 @@
 #include "sort.h"
 /**
- * buble_sort - Sorts an array
+ * bubble_sort - Sorts an array
  * @array: the array to be sorted
  * @size: the size of the array
-*/
-void bubble_sort(int *array, size_t size) {
-    size_t i, size2 = size;
-    int temp;
-    int done = 0;
-    while (done == 0) {
-        done = 1;
-    for (i = 0; i < size2 - 1; i++) {
-        if (array[i] > array[i + 1]) {
-            temp = array[i];
-            array[i] = array[i + 1];
-            array[i + 1] = temp;
-            print_array(array, size);
-            done = 0;
-        }
-    }
-    size2--;
-    }
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, size2 = size;
+	int temp;
+	int done = 0;
+
+	while (done == 0)
+	{
+		done = 1;
+		for (i = 0; i < size2 - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				print_array(array, size);
+				done = 0;
+			}
+		}
+		size2--;
+	}
 }
